8e682bdbc2db0285f28b2182b72b857fd90aed89
yan <33forester33@gmail.com>
2021-10-14 21:17:06 +0300

    рабочая

--- Калькулятор инвестора/Калькулятор инвестора.py
+++ Калькулятор инвестора/Калькулятор инвестора.py
@@ -1,125 +1,158 @@
-# сделать проверу всего алгоритма, сложив все компоненты и сравнив их с переменной cash
+# +++сделать проверу всего алгоритма, сложив все компоненты и сравнив их с переменной cash
 # +++ограничить в принтах вывод до сотых
-# позволить вводить в инпутах данные с любый количеством чисел после запятой, не использую саму запятую
-# сделать защиту от ввода неверных данных, символов, букв и пробелов
-# сделать возможность глобального подсчета, с нужноц корректировкой в определенную сторону. или автономный ввод, независящий от уже накопленного
-# число выводов не может быть отрицательным, как при вводи cashif   авным 10
+# +++позволить вводить в инпутах данные с любый количеством чисел после запятой, не использую саму запятую
+# +++сделать защиту от ввода неверных данных, символов, букв и пробелов
+# +++сделать возможность глобального подсчета, с нужноц корректировкой в определенную сторону. или автономный ввод, независящий от уже накопленного
+# +++число выводов не может быть отрицательным, как при вводи cashif   авным 10
 # +++ и не должно быть, что  вывод юсд 0, а еур больше 0. так как юсд в приоритете
-# при малом cash, когда есть юсд и бай, а евро нет, переходить на другой алгоритм не кратно 5, а кратно 1 в евро,
-# а бай уменьшать до 0. так как в приоритете купить валюту, а не сохранить в бай
-# дать возможность вводить как с запятой, так и без
-# создать функцию def и использовать для тренировки навыка
+# +++при малом cash, когда есть юсд и бай, а евро нет, переходить на другой алгоритм не кратно 5, а кратно 1 в евро,
+# +++а бай уменьшать до 0. так как в приоритете купить валюту, а не сохранить в бай
+# +++дать возможность вводить как с запятой, так и без
+# +++ создать функцию def и использовать для тренировки навыка
 # сделать лимит, после которого не откладывать бай
 # так как инпутов много, делать один ввод для этого меню
 # +++зациклить
-# если введен один 0 то на следующей этерации предлагать ввести только его, а на следующей ничего не предлагать
-#сделать зависимость от оконания (рубля, рублей)
+# +++если введен один 0 то на следующей этерации предлагать ввести только его, а на следующей ничего не предлагать
+# сделать зависимость от оконания (рубля, рублей)
 
-x = 0  # счетчик циклов
-main_eur = 0  # количество EUR
-
+loops = 0 #счетчик циклов
 while True:
-    """ФУНКЦИЯ ВВОДА ДАННЫХ И ИХ ПРОВЕРКИ"""
-    def control_input(text, vid, text_two, text_tree):
-        name = input(text)
-        while True:
-            try:
-                name_two = vid(name)
-                if name_two == 0:
-                    print(text_tree)
-                    break
-                elif name_two < 0:
-                    name = input("Число не может быть отрицательным\n" + text)
-                else:
-                    name_two = int(name_two)
-                    print(name_two, "рубля.")
-                    break
-            except ValueError:
-                name = input(text_two + text)
-        return name_two
+	flag = False #выход из вложенных циклов
 
+	'''___ФУНКЦИЯ ПОДБОРА ОКОНЧАНИЯ РУБЛЕЙ___(работает только с целыми числами'''
+	def endigs(index):
+		a1, a2, a3, a4 = 'рубль\n', 'рубля\n', 'рублей\n', '--------'
+		while True:
+			if index < 5:
+				if index == 0:
+					name = a3 + a4
+				elif index % 4 == 0:
+					name = a2 + a4
+				elif index % 3 == 0:
+					name = a2 + a4
+				elif index % 2 == 0:
+					name = a2 + a4
+				else:
+					name = a1 + a4
+				break
+			elif 5 <= index <= 20:
+				name = a3 + a4
+			else:
+				index = index % 10
+				continue
+			break
+		return name
 
-    # сбор данных
-    while True:
-        cash = control_input("Введи сумму предполагаемых инвестиций\n", float, "Это не число\n", "Нельзя инвестировать 0 рублей")
-        if cash > 0:
-            break
-    a = "Это не число\n"
-    b = "Не учитывается\n"
-    if x == 0:
-        usd_index = control_input("Курс USD\n", float, a, b)
-        eur_index = control_input("Курс EUR\n", float, a, b)
-    elif usd_index > 0 and eur_index == 0:
-        eur_index = control_input("Курс EUR\n", float, a, b)
-    elif eur_index > 0 and usd_index == 0:
-        usd_index = control_input("Курс USD\n", float, a, b)
+	"""ФУНКЦИЯ ВВОДА ДАННЫХ И ИХ ПРОВЕРКИ"""
+	def control_input(flag_two, text, typer, text_two, text_tree):
+		"""ФУНКЦИЯ ПРИВЕДЕНИЯ ЦЕЛОГО ЧИСЛА КУРСА В ВЕЩЕСТВЕННОЕ"""
+		def index_float(z):
+			if z[1] != '.':
+				z = z.replace('.', '').replace(',', '')
+			z = z[0], '.', z[1:]
+			z = str(z)[1:-1].replace(', ', '').replace("'", '')
+			return z
+		name = input(text)
+		while True:
+			try:
+				name_two = typer(name)
+				if name_two == 0:
+					print(text_tree)
+					break
+				elif name_two < 0:
+					name = input("Число не может быть отрицательным\n_________________________________\n\n" + text)
+				elif flag_two == 0:
+					name_two = typer(name_two)
+					print(name_two, endigs(name_two))#вывод если все правильно
+					break
+				else:
+					name_two = index_float(str(name_two))
+					print(name_two)
+					break
+			except ValueError:
+				name = input(text_two + text)
+		return typer(name_two)
+	# сбор данных
+	while True:
+		cash = control_input(0, "Введи сумму предполагаемых инвестиций\n", int, "Это не число\n____________\n\n",
+							 "Нельзя инвестировать 0 рублей")
+		if cash > 0:
+			break
+		elif cash == 0 and loops > 0:
+			flag = True
+			break
+	if flag:
+		loops = 0
+		print(loops)
+		continue
+	a, b = "Это не число\n", "Не учитывается\n"
+	if loops == 0:
+		usd_index = control_input(1, "Курс USD\n", float, a, b)
+		eur_index = control_input(1, "Курс EUR\n", float, a, b)
+	elif usd_index > 0 == eur_index and loops < 2:
+		eur_index = control_input("Курс EUR\n", float, a, b)
+	elif eur_index > 0 and usd_index == 0 and loops < 2:
+		usd_index = control_input("Курс USD\n", float, a, b)
 
+	# алгоритм определения размера поступления
+	if cash > 100:
+		a, b, c = 40, 30, 30
+	elif cash > 50:
+		a, b, c = 45, 35, 20
+	else:
+		a, b, c = 50, 40, 10
 
-    # алгоритм определения размера поступления
-    if cash > 100:
-        a, b = 40, 30
-    elif cash > 50:
-        a, b = 45, 20
-    else:
-        a, b = 50, 10
+	"""ФУНКЦИЯ ПРИВЕДЕНИЯ ОСТАТКА КРАТНОМУ ПЯТИ"""
+	def algo_in(price, cash_one, main):
+		if cash_one >= price:
+			if main % 5 >= 3:
+				main = (main // 5 * 5) + 5
+			else:
+				main = main // 5 * 5
+		return main
 
-    # алгоритм usd
-    if usd_index > 0 and eur_index > 0:  # Если ввел USD/EUR
-        main_usd = (cash / 100 * a) // usd_index
-        if main_usd % 5 >= 3:
-            main_usd = (main_usd // 5 * 5) + 5
-        else:
-            main_usd = main_usd // 5 * 5
-        if main_usd != 0:
-            print("Выйдет:", main_usd, "USD")
-    elif eur_index > 0:  # если ввел только EUR
-        usd_index = 0
-        main_usd = 0
+	"""ОСНОВНОЙ АЛГОРИТМ"""
+	def algoritm(index, index_two, letter, letter_two):
+		if index > 0 and index_two > 0:
+			main = cash / 100 * letter // index
+			main = algo_in(40, cash, main)
+		elif index > 0:
+			main = cash / 100 * (letter + letter_two) // index
+			main = algo_in(30, cash, main)
+			if cash < (main * index):
+				main -= 5
+		else:
+			main = 0
+		return main
 
-    # если ввел только USD
-    else:
-        if cash < 30:
-            main_usd = (cash / 100 * (100 - b)) // usd_index
-        else:
-            main_usd = (cash / 100 * a) // usd_index
-            if main_usd % 5 >= 3:
-                main_usd = (main_usd // 5 * 5) + 5
-            else:
-                main_usd = main_usd // 5 * 5
-        print("\nВыйдет", main_usd, "USD")
+	"""ФУНКЦИЯ ВЫВОДА ЕСЛИ >0"""
+	def print_out(main, simple_index):
+		if main != 0:
+			print(main, simple_index)
 
-    # алгоритм EUR
-    if usd_index > 0 and eur_index > 0:  # если ввел USD/EUR
-        main_eur = ((cash - (cash / 100 * (a + b))) - (main_usd * usd_index)) // eur_index
-        if main_eur % 5 >= 3.5:
-            main_eur = main_eur // 5 * 5 + 5
-        if cash - (main_usd * usd_index) - (main_eur * eur_index) < 0:
-            main_eur -= 5
-        else:
-            main_eur = main_eur // 5 * 5
-        if main_eur != 0:
-            print("Выйдет в EUR:", main_eur)
-    elif eur_index > 0:  # если ввел только EUR
-        main_eur = cash // eur_index
-        if main_eur > 0:
-            print("Выйдет в EUR:", main_eur)
+	# вычисление USD
+	main_usd = algoritm(usd_index, eur_index, a, b)
+	print_out(main_usd, "usd")
 
-        # алгоритм byn
-    if usd_index > 0 and eur_index > 0:
-        main_byn = cash - (main_usd * usd_index) - (main_eur * eur_index)
-        a = main_byn
-        main_byn = main_byn // 5 * 5
-        if main_byn != 0:
-            print("Отложить в BYN:", "%.2f" % main_byn)
+	# вычисление EUR
+	main_eur = algoritm(eur_index, usd_index, b, a)
+	print_out(main_eur, "eur")
 
-        # алгоритм остатка
-        if usd_index > 0 and eur_index > 0:
-            main_ost = (usd_index * main_usd) - (eur_index * main_eur) - main_byn
-        else:
-            main_ost = cash - (usd_index * main_usd) - (eur_index * main_eur)
-        if main_ost > 0:
-            print("Сумма остатка:", "%.2f" % main_ost)
-        else:
-            print("Ошибка")
-        print("\n=====================\n\n")
-    x += 1
+	# вычисление BYN
+	main_byn = cash - (usd_index * main_usd) - (eur_index * main_eur)
+	main_byn = algo_in(50, cash, main_byn)
+	if cash - (usd_index * main_usd) - (eur_index * main_eur) - main_byn < 0:
+		main_byn -= 5
+	if main_byn // 5 > 0:
+		main_byn = main_byn // 5 * 5
+	main_byn //= 1 * 1
+	print_out(main_byn, "BYN")
+
+	# вычисление остатка
+	main_ost = cash - (usd_index * main_usd) - (eur_index * main_eur) - main_byn
+	if main_ost >= 0:
+		print("Сумма остатка:", "%.2f" % main_ost, "рубля")
+	else:
+		print("Ошибка")
+	print("\n=====================\n\n")
+	loops += 1
